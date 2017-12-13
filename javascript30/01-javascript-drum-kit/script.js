736cec316be19ae3da7275ad120c4f0940e3be57
alex-aleman <alex.aleman.public@gmail.com>
2017-12-12 18:36:07 -0600

    Style is modified when key is pressed

--- javascript30/01-javascript-drum-kit/script.js
+++ javascript30/01-javascript-drum-kit/script.js
@@ -1,3 +1,11 @@
-window.addEventListener('keydown', (e) => {
-	console.log(e.keyCode);
+window.addEventListener('keydown', e => {
+	const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+	const key = document.querySelector(`div[data-key='${e.keyCode}']`);
+
+	if (audio) {
+		audio.currentTime = 0;
+		audio.play();
+
+			key.classList.toggle('playing');
+	}
 });
