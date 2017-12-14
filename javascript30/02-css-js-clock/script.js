743b82c87e98e74e2e0f89d82710852c1ff3f60c
alex-aleman <alex.aleman.public@gmail.com>
2017-12-13 20:41:49 -0600

    Set hands movement

---
+++ javascript30/02-css-js-clock/script.js
@@ -0,0 +1,13 @@
+const secondHand = document.querySelector('#second-hand');
+const minuteHand = document.querySelector('#minute-hand');
+const hourHand = document.querySelector('#hour-hand');
+
+setInterval(function () {
+	const now = new Date();
+	const seconds = (now.getSeconds() * 6) + 90;
+	const minutes = (now.getMinutes() * 6) + 90;
+	const hours = (now.getHours() * 30) + 90;
+	secondHand.style.transform = `rotate(${seconds}deg)`;
+	minuteHand.style.transform = `rotate(${minutes}deg)`;
+	hourHand.style.transform = `rotate(${hours}deg)`;
+}, 1000);
