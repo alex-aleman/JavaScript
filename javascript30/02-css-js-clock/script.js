b0a5852957221367f21b1b500b0ff41d86603a87
alex-aleman <alex.aleman.public@gmail.com>
2017-12-15 09:35:01 -0600

    xoed

--- javascript30/02-css-js-clock/script.js
+++ javascript30/02-css-js-clock/script.js
@@ -2,13 +2,13 @@ const secondHand = document.querySelector('#second-hand');
 const minuteHand = document.querySelector('#minute-hand');
 const hourHand = document.querySelector('#hour-hand');
 
-setInterval(function () {
+setInterval(() => {
 	const now = new Date();
 
 	const seconds = (now.getSeconds() * 6) + 90;
 	const minutes = (now.getMinutes() * 6) + 90;
 	const hours = (now.getHours() * 30) + 90;
-	
+
 	secondHand.style.transform = `rotate(${seconds}deg)`;
 	minuteHand.style.transform = `rotate(${minutes}deg)`;
 	hourHand.style.transform = `rotate(${hours}deg)`;
