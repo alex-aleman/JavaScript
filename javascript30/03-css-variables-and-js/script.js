dd12a63529533cb5b64d036d084ed6f37e9013d0
alex-aleman <alex.aleman.public@gmail.com>
2017-12-14 21:30:37 -0600

    Added events

---
+++ javascript30/03-css-variables-and-js/script.js
@@ -0,0 +1,8 @@
+const inputs = document.querySelectorAll('input');
+
+function handleUpdate() {
+	console.log(this.value);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
