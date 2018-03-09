55b37f6340e37d21461f5f39960043a2643c4f06
alex-aleman <alex.aleman.public@gmail.com>
2018-03-08 22:06:11 -0600

    Armazón 06

--- javascript30/06-ajax-type-ahead/script.js
+++ javascript30/06-ajax-type-ahead/script.js
@@ -1,1 +1,33 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const cities = [];
+
+fetch(endpoint)
+	.then(blob => blob.json())
+	.then(data => cities.push(...data));
+
+function findMatches(word, cities) {
+	return cities.filter(place => {
+		const regex = new RegExp(word, 'gi');
+		return place.city.match(regex) || place.state.match(regex);
+	});
+}
+
+function displayMatches() {
+	const matchArray = findMatches(this.value, cities);
+	const html = matchArray.map(place => {
+		return `
+			<li>
+				<span class="name">${place.city}, ${place.state}</span>
+				<span class="population">${place.population}</span>
+			</li>
+		`;
+	}).join('');
+	suggestions.innerHTML = html;
+}
+
+const search = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+search.addEventListener('change', displayMatches);
+search.addEventListener('keyup', displayMatches);
