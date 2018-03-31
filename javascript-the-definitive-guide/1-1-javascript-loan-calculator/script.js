b92cbac5779f2006b81888c1aa54dd0833330843
alex-aleman <alex.aleman.public@gmail.com>
2018-03-30 20:52:48 -0600

    mas variables

--- javascript-the-definitive-guide/1-1-javascript-loan-calculator/script.js
+++ javascript-the-definitive-guide/1-1-javascript-loan-calculator/script.js
@@ -1,11 +1,15 @@
 "use strict";
 
 function calculate() {
-	var amount = document.getElementById("amount");
-	var apr = document.getElementById("apr");
-	var years = document.getElementById("years");
-	var zipcode = document.getElementById("zipcode");
-	var payment = document.getElementById("payment");
-	var total = document.getElementById("total");
-	var totalinterest = document.getElementById("totalinterest");
+	const amount = document.getElementById("amount");
+	const apr = document.getElementById("apr");
+	const years = document.getElementById("years");
+	const zipcode = document.getElementById("zipcode");
+	const payment = document.getElementById("payment");
+	const total = document.getElementById("total");
+	const totalinterest = document.getElementById("totalinterest");
+
+	const principal = parseFloat(amount.value);
+	const interest = parseFloat(apr.value) / 100 / 12;
+	const payments = parseFloat(years.value) * 12;
 }
