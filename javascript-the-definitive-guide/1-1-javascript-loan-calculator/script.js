fcdbc846654d92bb13aaeda5007e805cfd228566
alex-aleman <alex.aleman.public@gmail.com>
2018-03-30 20:49:29 -0600

    variables

---
+++ javascript-the-definitive-guide/1-1-javascript-loan-calculator/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function calculate() {
+	var amount = document.getElementById("amount");
+	var apr = document.getElementById("apr");
+	var years = document.getElementById("years");
+	var zipcode = document.getElementById("zipcode");
+	var payment = document.getElementById("payment");
+	var total = document.getElementById("total");
+	var totalinterest = document.getElementById("totalinterest");
+}
